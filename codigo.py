d312fdf93d608cee869e8d39c72e9326370350a8
Carolina Arriaga Galaviz <c.arriagag03@gmail.com>
2023-11-12 12:31:19 -0600

    Correcciones

--- codigo.py
+++ codigo.py
@@ -79,28 +79,27 @@ def saldo_clientes(mensaje):
     print("Por favor, ingresa un número válido.")
     return presupuesto_ventas(mensaje)
     
-    
-    saldo_clientes_31_dic_2022 = float(input("Ingrese el saldo de clientes al 31 de diciembre de 2022: "))
-    ventas_2023 = presupuesto_ventas()
+saldo_clientes_31_dic_2022 = float(input("Ingrese el saldo de clientes al 31 de diciembre de 2022: "))
+ventas_2023 = total_ventas_3erp2sem
 
-    total_clientes_2023 = saldo_clientes_31_dic_2022 + sum(ventas_2023)
+total_clientes_2023 = saldo_clientes_31_dic_2022 + total_ventas_año
 
-    por_cobranza_2022 = float(input("Ingrese el monto por cobranza del 2022: "))
-    por_cobranza_2023 = float(input("Ingrese el monto por cobranza del 2023: "))
-    total_entradas = por_cobranza_2022 + por_cobranza_2023
+por_cobranza_2022 = float(input("Ingrese el monto por cobranza del 2022: "))
+por_cobranza_2023 = total_ventas_año * 0.80
+total_entradas = por_cobranza_2022 + por_cobranza_2023
 
-    saldo_clientes_2023 = total_clientes_2023 - total_entradas
+saldo_clientes_2023 = total_clientes_2023 - total_entradas
 # Crear una lista de listas con los datos del presupuesto de requerimiento de materiales
 datos_saldo_clientes= [
-    ["Saldo de clientes 31-DIC-2022", "", saldo_clientes_31_dic_2022],
-    ["Ventas 2023", "", ventas_2023],
-    ["Total de clientes 2023", "" , total_clientes_2023],
-    ["Entradas de Efectivo", "" , "",],
-    ["Por cobranza del 2022", por_cobranza_2022 , ""],
-    ["Por cobranza del  2023", por_cobranza_2023 , ""],
-    ["Total de Entradas 2023", "", total_entradas],
-    ["Saldo de clientes del 2023", "" , saldo_clientes_2023]
-]
+  ["Saldo de clientes 31-DIC-2022", "", saldo_clientes_31_dic_2022],
+  ["Ventas 2023", "", ventas_2023],
+  ["Total de clientes 2023", "" , total_clientes_2023],
+  ["Entradas de Efectivo", "" , "",],
+  ["Por cobranza del 2022", por_cobranza_2022 , ""],
+  ["Por cobranza del  2023", por_cobranza_2023 , ""],
+  ["Total de Entradas 2023", "", total_entradas],
+  ["Saldo de clientes del 2023", "" , saldo_clientes_2023]
+  ]
 
 # Impresion de datos de presupuesto de requerimiento de materiales - Tabla 2
 tabla = tabulate(datos_saldo_clientes, headers=["Descripción", "Importe", "Total"], tablefmt="fancy_grid")
@@ -116,58 +115,58 @@ def presupuesto_produccion(mensaje):
   except ValueError:
     print("Por favor, ingresa un número válido.")
     return presupuesto_produccion(mensaje)
-    #producto 1
-    produccion_unidades_1erp1ersem = int(input("Ingrese la cantidad de unidades a producir del 1er producto en el primer semestre : "))
-    inventario_final_deseado_1erp1ersem = int(input("Ingrese el inventario final deseado del 1er producto en el primer semestre : "))
-    total_unidades_1erp1ersem = produccion_unidades_1erp1ersem + inventario_final_deseado_1erp1ersem
-    inventario_inicial_1erp1ersem = int(input("Ingrese el inventario inicial del 1er producto en el primer semestre : "))
-    unidades_requeridas_1erp1ersem = produccion_unidades_1erp1ersem + inventario_final_deseado_1erp1ersem - inventario_inicial_1erp1ersem
-    
-    produccion_unidades_1erp2dosem = int(input("Ingrese la cantidad de unidades a producir del 1er producto en el segundo semestre : "))
-    inventario_final_deseado_1erp2dosem = int(input("Ingrese el inventario final deseado del 1er producto en el segundo semestre : "))
-    total_unidades_1erp2dosem = produccion_unidades_1erp2dosem + inventario_final_deseado_1erp2dosem
-    inventario_inicial_1erp2dosem = int(input("Ingrese el inventario inicial del 1er producto en el segundo semestre : "))
-    unidades_requeridas_1erp2dosem = produccion_unidades_1erp2dosem + inventario_final_deseado_1erp2dorsem - inventario_inicial_1erp2dosem
-    total_unidades_producir_1erp = unidades_requeridas_1erp1ersem + unidades_requeridas_1erp2dosem
-    total_unidades_1erp = total_unidades_1erp1ersem + total_unidades_1erp2dosem
-    
-    # producto 2
-    produccion_unidades_2dop1ersem = int(input("Ingrese la cantidad de unidades a producir del 2do producto en el primer semestre : "))
-    inventario_final_deseado_2dop1ersem = int(input("Ingrese el inventario final deseado del 2do producto en el primer semestre : "))
-    total_unidades_2dop1ersem = produccion_unidades_2dop1ersem + inventario_final_deseado_2dop1ersem
-    inventario_inicial_2dop1ersem = int(input("Ingrese el inventario inicial del 2do producto en el primer semestre : "))
-    unidades_requeridas_2dop1ersem = produccion_unidades_2dop1ersem + inventario_final_deseado_2dop1ersem - inventario_inicial_2dop1ersem
-    
-    produccion_unidades_2dop2dosem = int(input("Ingrese la cantidad de unidades a producir del 2do producto en el segundo semestre : "))
-    inventario_final_deseado_2dop2dorsem = int(input("Ingrese el inventario final deseado del 2do producto en el segundo semestre : "))
-    total_unidades_2dop2dosem = produccion_unidades_2dop2dosem + inventario_final_deseado_2dop2dosem
-    inventario_inicial_2dop2dosem = int(input("Ingrese el inventario inicial del 2do producto en el segundo semestre : "))
-    unidades_requeridas_2dop2dosem = produccion_unidades_2dop2dosem + inventario_final_deseado_2dop2dorsem - inventario_inicial_2dop2dosem
-    total_unidades_producir_2dop = unidades_requeridas_2dop1ersem + unidades_requeridas_2dop2dosem
-    total_unidades_2dop = total_unidades_2dop1ersem + total_unidades_2dop2dosem
-    
-    #producto 3
-    produccion_unidades_3erp1ersem = int(input("Ingrese la cantidad de unidades a producir del 3er producto en el primer semestre : "))
-    inventario_final_deseado_3erp1ersem = int(input("Ingrese el inventario final deseado del 3er producto en el primer semestre : "))
-    total_unidades_2dop1ersem = produccion_unidades_3erp1ersem + inventario_final_deseado_3erp1ersem
-    inventario_inicial_3erp1ersem = int(input("Ingrese el inventario inicial del 3er producto en el primer semestre : "))
-    unidades_requeridas_3erp1ersem = produccion_unidades_3erp1ersem + inventario_final_deseado_3erp1ersem - inventario_inicial_3erp1ersem
-    
-    produccion_unidades_3erp2dosem = int(input("Ingrese la cantidad de unidades a producir del 3er producto en el segundo semestre : "))
-    inventario_final_deseado_3erp2dorsem = int(input("Ingrese el inventario final deseado del 3er producto en el segundo semestre : "))
-    total_unidades_2dop1ersem = produccion_unidades_3erp2dosem + inventario_final_deseado_3erp2dosem
-    inventario_inicial_3erp2dosem = int(input("Ingrese el inventario inicial del 3er producto en el segundo semestre : "))
-    unidades_requeridas_3erp2dosem = produccion_unidades_3erp2dosem + inventario_final_deseado_3erp2dorsem - inventario_inicial_3erp2dosem
-    total_unidades_producir_3erp = unidades_requeridas_3erp1ersem + unidades_requeridas_3erp2dosem
-    total_unidades_3erp = total_unidades_3erp1ersem + total_unidades_3erp2dosem
-    
-    return total_unidades_producir_1erp, total_unidades_producir_2dop, total_unidades_producir_3erp
+  
+#producto 1
+produccion_unidades_1erp1ersem = int(input("Ingrese la cantidad de unidades a producir del 1er producto en el primer semestre : "))
+inventario_final_deseado_1erp1ersem = int(input("Ingrese el inventario final deseado del 1er producto en el primer semestre : "))
+total_unidades_1erp1ersem = produccion_unidades_1erp1ersem + inventario_final_deseado_1erp1ersem
+inventario_inicial_1erp1ersem = int(input("Ingrese el inventario inicial del 1er producto en el primer semestre : "))
+unidades_requeridas_1erp1ersem = produccion_unidades_1erp1ersem + inventario_final_deseado_1erp1ersem - inventario_inicial_1erp1ersem
+
+produccion_unidades_1erp2dosem = int(input("Ingrese la cantidad de unidades a producir del 1er producto en el segundo semestre : "))
+inventario_final_deseado_1erp2dosem = int(input("Ingrese el inventario final deseado del 1er producto en el segundo semestre : "))
+total_unidades_1erp2dosem = produccion_unidades_1erp2dosem + inventario_final_deseado_1erp2dosem
+inventario_inicial_1erp2dosem = int(input("Ingrese el inventario inicial del 1er producto en el segundo semestre : "))
+unidades_requeridas_1erp2dosem = produccion_unidades_1erp2dosem + inventario_final_deseado_1erp2dosem - inventario_inicial_1erp2dosem
+total_unidades_producir_1erp = unidades_requeridas_1erp1ersem + unidades_requeridas_1erp2dosem
+total_unidades_1erp = total_unidades_1erp1ersem + total_unidades_1erp2dosem
+
+# producto 2
+produccion_unidades_2dop1ersem = int(input("Ingrese la cantidad de unidades a producir del 2do producto en el primer semestre : "))
+inventario_final_deseado_2dop1ersem = int(input("Ingrese el inventario final deseado del 2do producto en el primer semestre : "))
+total_unidades_2dop1ersem = produccion_unidades_2dop1ersem + inventario_final_deseado_2dop1ersem
+inventario_inicial_2dop1ersem = int(input("Ingrese el inventario inicial del 2do producto en el primer semestre : "))
+unidades_requeridas_2dop1ersem = produccion_unidades_2dop1ersem + inventario_final_deseado_2dop1ersem - inventario_inicial_2dop1ersem
+
+produccion_unidades_2dop2dosem = int(input("Ingrese la cantidad de unidades a producir del 2do producto en el segundo semestre : "))
+inventario_final_deseado_2dop2dorsem = int(input("Ingrese el inventario final deseado del 2do producto en el segundo semestre : "))
+total_unidades_2dop2dosem = produccion_unidades_2dop2dosem + inventario_final_deseado_2dop2dorsem
+inventario_inicial_2dop2dosem = int(input("Ingrese el inventario inicial del 2do producto en el segundo semestre : "))
+unidades_requeridas_2dop2dosem = produccion_unidades_2dop2dosem + inventario_final_deseado_2dop2dorsem - inventario_inicial_2dop2dosem
+total_unidades_producir_2dop = unidades_requeridas_2dop1ersem + unidades_requeridas_2dop2dosem
+total_unidades_2dop = total_unidades_2dop1ersem + total_unidades_2dop2dosem
+
+#producto 3
+produccion_unidades_3erp1ersem = int(input("Ingrese la cantidad de unidades a producir del 3er producto en el primer semestre : "))
+inventario_final_deseado_3erp1ersem = int(input("Ingrese el inventario final deseado del 3er producto en el primer semestre : "))
+total_unidades_3erp1ersem = produccion_unidades_3erp1ersem + inventario_final_deseado_3erp1ersem
+inventario_inicial_3erp1ersem = int(input("Ingrese el inventario inicial del 3er producto en el primer semestre : "))
+unidades_requeridas_3erp1ersem = produccion_unidades_3erp1ersem + inventario_final_deseado_3erp1ersem - inventario_inicial_3erp1ersem
+
+produccion_unidades_3erp2dosem = int(input("Ingrese la cantidad de unidades a producir del 3er producto en el segundo semestre : "))
+inventario_final_deseado_3erp2dorsem = int(input("Ingrese el inventario final deseado del 3er producto en el segundo semestre : "))
+total_unidades_3erp2dosem = produccion_unidades_3erp2dosem + inventario_final_deseado_3erp2dorsem
+inventario_inicial_3erp2dosem = int(input("Ingrese el inventario inicial del 3er producto en el segundo semestre : "))
+unidades_requeridas_3erp2dosem = produccion_unidades_3erp2dosem + inventario_final_deseado_3erp2dorsem - inventario_inicial_3erp2dosem
+total_unidades_producir_3erp = unidades_requeridas_3erp1ersem + unidades_requeridas_3erp2dosem
+total_unidades_3erp = total_unidades_3erp1ersem + total_unidades_3erp2dosem
+
 
 # Crear una lista de listas con los datos del presupuesto de requerimiento de materiales
 datos_presupuesto_produccion= [
     ["PRODUCTO 1", "", "", ""],
     ["Unidades a vender", produccion_unidades_1erp1ersem, produccion_unidades_1erp2dosem , total_unidades_producir_1erp],
-    ["Inventario Final", inventario_final_deseado_1erp1ersem , inventario_final_deseado_1erp2dorsem , inventario_final_deseado_1erp2dorsem],
+    ["Inventario Final", inventario_final_deseado_1erp1ersem , inventario_final_deseado_1erp2dosem , inventario_final_deseado_1erp2dosem],
     ["Total de unidades", total_unidades_1erp1ersem , total_unidades_1erp2dosem , total_unidades_1erp],
     ["Inventario Inicial", inventario_inicial_1erp1ersem , inventario_inicial_1erp2dosem , inventario_inicial_1erp2dosem],
     ["Unidades a Produccir", unidades_requeridas_1erp1ersem , unidades_requeridas_1erp2dosem , total_unidades_producir_1erp],
