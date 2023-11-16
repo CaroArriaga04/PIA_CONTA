ca8913a39f9abca2dc3523455ece6a78bbd71e55
Carolina Arriaga Galaviz <c.arriagag03@gmail.com>
2023-11-15 18:11:26 -0600

    Terminado

--- presupuesto.py
+++ presupuesto.py
@@ -996,4 +996,159 @@ Estado_Costo_P_V = [
   ["Inventario Final de Productos Terminados ",total_de_costos],
   [" Costo de Ventas",costo_ventas_fin],
 ]
-print(tabulate(Estado_Costo_P_V, headers=["Presupuesto del X al X de 20XX"], tablefmt="fancy_grid"))
+print(tabulate(Estado_Costo_P_V, headers=["Presupuesto del X al X de 20XX",""], tablefmt="fancy_grid"))
+
+#Tabla 13 Estado de resultados
+def estado_resultados (mensaje):
+  try:
+    dato = float(input(mensaje))
+    return dato
+  except ValueError:
+    print("Por favor, ingresa un número válido.")
+    return inventarios_finales(mensaje)
+
+total_ventas_año = total_ventas_1sem + total_ventas_2sem 
+costo_ventas= total_produccion - total_de_costos
+utilidad_bruta=float(input("Ingrese la utilidad bruta: "))
+gastos_operacion= total_gastos_operacion1 + total_gastos_operacion2 
+utilidad_operacion= utilidad_bruta - gastos_operacion
+isr=float(input("Ingresa el ISR: "))
+ptu=float(input("Ingresa el PTU: "))
+utilidad_neta= utilidad_operacion - isr - ptu
+
+estado_de_resultados=[
+    ["\nEstado de resultados"],
+    ["Ventas: ", total_ventas_año],
+    ["Costo de ventas: ",costo_ventas],
+    ["Utilidad bruta: ",utilidad_bruta],
+    ["Gastos de operación: ",gastos_operacion],
+    ["Utilidad de operación: ",utilidad_operacion],
+    ["ISR",isr],
+    ["PTU",ptu],
+    ["Utilidad neta",utilidad_neta],
+]
+
+print(tabulate(estado_de_resultados, headers=["Presupuesto del X al X de 20XX",""], tablefmt="fancy_grid"))
+
+#Tabla 14 Estado de flujo de efectivo
+def estado_flujo_efectivo(mensaje):
+  try:
+    dato = float(input(mensaje))
+    return dato
+  except ValueError:
+    print("Por favor, ingresa un número válido.")
+    return inventarios_finales(mensaje)
+
+saldo_inicial=float(input("Ingresa el saldo inicial: "))
+por_cobranza_2022
+por_cobranza_2023
+total_entrada= por_cobranza_2022 + por_cobranza_2023
+efectivo_dispo= saldo_inicial + por_cobranza_2022 + por_cobranza_2023
+saldo_proveedores_31_dic_año
+por_proveedor_año1
+pago_mano_obra = total_mod_sem1 + total_mod_sem2 + total_mod_año
+gastos_indirectos= total_gif1 + total_gif2 + total_gif3
+depreciacion= depreciacion1 + depreciacion2 + depreciacion3
+gastos_indirectos_fabricacion=gastos_indirectos - depreciacion
+total_gastos= total_gastos_operacion1 + total_gastos_operacion2 + total_gastos_operacion3
+depre=depreciacion1 + depreciacion2 + depreciacion3
+gastos_operacion= total_gastos - depre
+activo_fijo=float(input("Ingresa el activo fijo: "))
+isr2=float(input("Ingresa el ISR de la redaccion: "))
+isr
+total_salidas=saldo_proveedores_31_dic_año + por_proveedor_año1 + pago_mano_obra + gastos_indirectos_fabricacion + gastos_operacion + activo_fijo + isr2 + isr 
+flujo_efectivo = efectivo_dispo - total_salidas
+
+
+estado_de_flujo_efectivo=[
+    ["\nEstado de flujo efectivo"],
+    ["Saldo inicial de efectivo",saldo_inicial],
+    ["Entradas:"],
+    ["Cobranza del 2022", por_cobranza_2022],
+    ["Cobranza del 2023", por_cobranza_2023],
+    ["Total de entradas",total_entrada],
+    ["Efectivo disponible",efectivo_dispo], 
+    ["Salidas:"],
+    ["Proveedores 2023", saldo_proveedores_31_dic_año],
+    ["Proveedores 2022",por_proveedor_año1],
+    ["Pago mano de obra directa", pago_mano_obra],
+    ["Pago gastos indirectos de fabricación", gastos_indirectos_fabricacion],
+    ["Pago de gastos de operación", gastos_operacion],
+    ["Compra de activo fijo (maquinaria)",activo_fijo],
+    ["Pago ISR 2022", isr2],
+    ["Pago ISR 2023",isr],
+    ["Total de salidas", total_salidas],
+    ["Flujo de efectivo actual", flujo_efectivo ],  
+]
+
+print(tabulate(estado_de_flujo_efectivo, headers=["Presupuesto del X al X de 20XX",""], tablefmt="fancy_grid"))
+
+
+#Tabla 15 balance general
+def balance_general(mensaje):
+  try:
+    dato = float(input(mensaje))
+    return dato
+  except ValueError:
+    print("Por favor, ingresa un número válido.")
+    return inventarios_finales(mensaje)
+
+
+total_activos_circulante= flujo_efectivo + saldo_clientes_2023 + invent_final + total_de_costos
+terreno=float(input("Ingresa la cantidad de terreno: "))
+planta_equipo=float(input("Ingrese la cantidad de planta y equipo: "))
+documentos_por_pagar= float(input("Ingrese la cantidad de documentos por pagar: "))
+total_pasivo_corto_plazo= saldo_proveedores_año + documentos_por_pagar + ptu
+obligaciones_por_pagar=float(input("Ingrese la cantidad de obligaciones por pagar: "))
+pasivo_total= obligaciones_por_pagar + total_pasivo_corto_plazo
+capital_aportado=float(input("Ingrese la cantidad de capital aportado: "))
+capital_ganado=float(input("Ingrese la cantidad de capital ganado: "))
+total_de_capital_contable=capital_ganado + capital_aportado + utilidad_neta
+suma_pasivo_capital= pasivo_total + total_de_capital_contable 
+
+
+balance_general1=[
+    ["\n "],
+    ["ACTIVO"],
+    ["Circulante"],
+    ["Efectivo: ",flujo_efectivo], 
+    ["Clientes: ",saldo_clientes_2023],
+    ["Inventario de materiales: ",invent_final],
+    ["Inventario de producto terminado: ",total_de_costos],
+    ["Total de activos circulantes: ", total_activos_circulante],
+
+    ["No Circulante"],
+    ["Terreno: ", terreno],
+    ["Planta y equipo: ", planta_equipo],
+    ["Depreciacion acumulada:  ", ],
+    ["Total activos no circulante "],
+
+    ["ACTIVO TOTAL: "],
+
+    ["PASIVO: "],
+    ["Corto plazo: "],
+    ["Proveeedores: ", saldo_proveedores_año],
+    ["Documentos por pagar: ", documentos_por_pagar],
+    ["ISR por pagar: ", isr],
+    ["PTU por pagar: ", ptu],
+    ["Total de Pasivo Corto Plazo:", total_pasivo_corto_plazo],
+
+    ["Largo plazo"],
+    ["Obligaciones por pagar: ", obligaciones_por_pagar],
+    ["Total de Pasivo Largo Plazo: ", obligaciones_por_pagar],
+
+
+    ["PASIVO TOTAL", pasivo_total],
+
+    ["CAPITAL CONTABLE"],
+    ["Capital aportado",capital_aportado],
+    ["Capital ganado", capital_ganado],
+    ["Utilidad del ejercicio", utilidad_neta],
+    ["Total de Capital Contable", total_de_capital_contable],
+
+    ["SUMA DE PASIVO Y CAPITAL",suma_pasivo_capital],
+]
+
+print(tabulate(balance_general1, headers=["Balange General",""], tablefmt="fancy_grid"))
+
+
